c24310b1dec8b31ddfdccc84a2d1ea06e7497f69
blachkov <54777701+blachkov@users.noreply.github.com>
2019-09-01 22:48:15 +0300

    Update cutter.sh

--- cutter.sh
+++ cutter.sh
@@ -4,7 +4,7 @@ number_of_lines=20
 n=$number_of_lines
 file_lines_count=$(wc -l < clients.csv)
 echo $file_lines_count
-for (( c=1; c<=$file_lines_count; c+=$number_of_lines ))
+for (( c=1; c<=$file_lines_count; c=$number_of_lines ))
 do
     sed -n '$c,$n' clients.csv > clients1.csv
     n+=number_of_lines
