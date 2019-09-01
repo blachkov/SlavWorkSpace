250778ef7563f80e52c0aa98e2eb6bd6b401f199
blachkov <slaviblachkov@gmail.com>
2019-09-01 22:28:39 +0300

    adding cutter.sh

---
+++ cutter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+number_of_lines=20
+n=$number_of_lines
+file_lines_count=$(wc -l < clients.csv)
+echo $file_lines_count
+for (( c=1; c<=$file_lines_count; c+=$number_of_lines ))
+do
+    sed -n '$c,$n' clients.csv > clients1.csv
+    n+=number_of_lines
+done
